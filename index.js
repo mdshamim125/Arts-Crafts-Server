da836f66957f8596b59809059197a9cd84b89c30
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-04-26 23:50:49 +0600

    get method for craft done

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://cse12005038brur:j3okhLObb3jLTwlX@cluster0.s1le0vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const craftItemCollection = client.db("Art-Craft").collection("craftItem");
+
+    app.get("/craft", async (req, res) => {
+      const cursor = craftItemCollection.find();
+      const result = await cursor.toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Art & Craft Server is running.....");
