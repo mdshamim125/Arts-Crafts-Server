b70c5a7d655a3ec4d667b6895f0bc6d15328c59f
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-04-28 00:08:17 +0600

    getMethod by id, email and deleteMethod done, alhamdulillah

--- index.js
+++ index.js
@@ -34,13 +34,26 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/craft/:id', async (req, res) => {
+    app.get("/craft/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) }
+      const query = { _id: new ObjectId(id) };
       const result = await craftItemCollection.findOne(query);
       res.send(result);
-  })
+    });
 
+    app.get("/myCraft/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await craftItemCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete('/myCraft/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await craftItemCollection.deleteOne(query);
+      res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
