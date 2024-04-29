105ef232c751f7cc5ebde8838d4ccd5d3385ac97
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-04-29 12:44:35 +0600

    environment setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
 const uri =
-  "mongodb+srv://cse12005038brur:j3okhLObb3jLTwlX@cluster0.s1le0vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s1le0vj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -72,7 +74,7 @@ async function run() {
       res.send(result);
     });
 
-    
+
     app.put("/myCraft/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
